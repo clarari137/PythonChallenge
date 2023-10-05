3c212ffdd70e1084ddd9752e4bfc396e00f5edb4
clarari137 <142682830+clarari137@users.noreply.github.com>
2023-10-05 10:43:28 -0500

    Add files via upload

---
+++ PyBank/main.py
@@ -0,0 +1,54 @@
+# read in budget__data.csv, skip header
+import os
+import csv
+
+# define
+total = 0
+months = []
+profits = []
+incdec = []
+
+csvpath = os.path.join('Resources','budget_data.csv')
+outpath = os.path.join('Analysis', 'pybank_output.txt')
+
+with open(outpath, 'w') as outfile:
+    outfile.write("PyBank Financial analysis\n ---------------------------------------\n")
+
+    with open(csvpath, encoding='UTF-8') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
+        csv_header = next(csvfile)
+
+        for row in csvreader:
+            # total number of months by appending all dates (months) in the dataset to months list
+            months.append(row[0])
+            #total profits/losses over the period by adding all values in the second column
+            total += int(row[1])
+            #changes in profits/losses by creating new list containing the dollar values
+            profits.append(int(row[1]))
+        #find the total changes and then the average
+        dif = profits[85]-profits[0]
+        avg = dif/85
+
+            #greatest increase in profits
+            #greatest decrease in profits
+        for x in range(1,85):
+            difference = profits[x]-profits[x-1]
+            incdec.append(difference)
+        
+        #Print final answers
+    print(len(months))
+    print(total)
+    print(dif)
+    print(avg)
+    print(f'{months[incdec.index(int(max(incdec)))+1]} ({max(incdec)})')
+    print(f'{months[incdec.index(int(min(incdec)))+1]} ({min(incdec)})')
+                
+        #Compile final answers in txt file
+    outfile.write(f'Total months in dataset: {len(months)}\n')
+    outfile.write(f'Total profits/losses over the period: ${total}\n')
+    outfile.write(f'Change in profits/losses: ${dif}\n')
+    outfile.write(f'Average change: ${avg:.2f}\n')
+    outfile.write(f'Greatest increase: {months[incdec.index(int(max(incdec)))+1]} (${max(incdec)})\n')
+    outfile.write(f'Greatest decrease: {months[incdec.index(int(min(incdec)))+1]} (${min(incdec)})')
+
+        
---
+++ PyPoll/main.py
@@ -0,0 +1,64 @@
+# read in election_data.csv
+import os
+import csv
+
+# define
+voterid = []
+candidates = []
+filtered_cand = []
+candidate_votes = 0
+winner_votes = 0
+winner_name = ""
+candidate_name = []
+
+csvpath = os.path.join('Resources','election_data.csv')
+outpath = os.path.join('Analysis', 'pypoll_output.txt')
+
+# need nested with so that I can write to the ouput document?
+
+with open(outpath, 'w') as outfile:
+    outfile.write("PyPoll Election Results\n-------------------------\n")
+
+    with open(csvpath, encoding='UTF-8') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
+        csv_header = next(csvfile)
+
+        for row in csvreader:
+            # make list of all voter IDs to count ballots cast
+            voterid.append(row[0])
+            # complete list of candidates
+            candidates.append(row[2])
+        
+        outfile.write(f'Total votes cast: {len(voterid)}\n' '-------------------------\n')
+        print(f'Total votes cast: {len(voterid)}')
+
+    # outside of the for loop so it doesn't run forever
+    # first sort alphabetically (link from BCS)
+    # then append first candidate to list
+    # run through each candidate in the list and add to filtered list
+    # print filtered list
+        candidates.sort()  
+        filtered_cand.append(candidates[0])
+        for i in range(0,int(len(candidates))-1):
+            if candidates[i] != candidates[i+1]:
+                filtered_cand.append(candidates[i+1])
+        print(filtered_cand)
+
+            # percent votes won
+            # total votes won
+        for fc in filtered_cand:
+        # for i in range(0,len(filtered_cand)):
+            for candi in candidates:
+                if fc == candi:
+                    candidate_votes += 1
+            outfile.write(f"{fc}: {int(candidate_votes)/int(len(voterid)):.3%}, ({candidate_votes})\n")
+            print(f"{fc}: {int(candidate_votes)/int(len(voterid)):.3%}, ({candidate_votes})")
+            # Winner
+            if candidate_votes > winner_votes:
+                winner_votes = candidate_votes
+                winner_name = fc
+            candidate_votes = 0
+        print(f'Winner: {winner_name}')
+    outfile.write('-------------------------\n'f'Winner: {winner_name}')
+
+        
